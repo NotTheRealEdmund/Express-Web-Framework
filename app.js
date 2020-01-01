080a50fa791372648396dc25b08fd047973641d1
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-01 18:24:48 +0800

    Add comments

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 
+// Default route
 app.get('/', function(req, res) {
     res.send("Hello world!");
 });
 
+// Add '/example' route
 app.get('/example', (req,res) => {     
     res.send('Hitting example route');
 });
 
+// Add route parameters
 app.get('/example/:name/:age', (req,res) => {     
     res.send(req.params.name + " : " + req.params.age);
+    // Get query string values
     console.log(req.query);
 });
 
 app.listen(3000);
+
+// Use route parameters when you absolutely must have that data
+// Use query strings when you want to give some optional settings
