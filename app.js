bc35f6a178597854be7ccded70d7df044c2cd687
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-01 17:52:10 +0800

    Adding another route to the same address

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express();
 app.get('/', function(req, res) {
     res.send("Hello world!");
 });
- 
+
+app.get('/example', (req,res) => {     
+    res.send('Hitting example route');
+});
+
 app.listen(3000);
