8950b9c94789c0a5ef54c0c92e43e5f9002744f9
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-01-03 22:56:52 +0800

    Use middleware functions to modify the request object and the response object

--- middleware.js
+++ middleware.js
@@ -1,17 +1,14 @@
 const express = require('express');  
 const app = express();
 
-app.use('/example', (req, res, next) => {       // Now only the route '/example' can execute middleware    
-    console.log(req.url, req.method);
+app.use((req, res, next) => {  
+    req.banana = 'banana';      // Take the user's request and add a property of banana and assign it a value of 'banana'
     next();        
 });
 
 app.get('/', (req,res) => {
+    console.log(req.banana);    
     res.send('Hello World!');
 });
 
 app.listen(3000);
-
-// localhost:3000 will show 'Hello World!' in browser but nothing in VS code cmd
-// localhost:3000/example will show 'Cannot GET /example' in browser but in VS code cmd you'll see '/ GET' which means
-// middleware still worked.
